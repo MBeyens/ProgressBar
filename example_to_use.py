d1e0f50f76b6d75e956e08820dc83173412b63e3
Matthias Beyens <matthias.beyens@uantwerpen.be>
2017-05-10 11:03:57 +0200

    Create example_to_use.py

---
+++ example_to_use.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import progress_tracker
+
+amount_total_variants = 123
+variant_number = 0
+for variant_number in range(0:amount_total_variants):
+    progress.update_progress(variant_number, amount_total_variants)
